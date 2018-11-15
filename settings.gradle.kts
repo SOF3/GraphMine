47c9af2a9a3a1a08219f646c1c4cbf12d01d1656
SOFe <sofe2038@gmail.com>
2018-11-15 15:54:04 +0800

    Merged graphmine-api and graphmine-core gradle projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,7 @@
 rootProject.name = "graphmine"
 
-include("graphmine-core")
-include("graphmine-cli")
-include("graphmine-api")
 include("graphmine-util")
 include("graphmine-i18n")
 include("graphmine-i18n-core")
+include("graphmine-core")
+include("graphmine-cli")
